219c96af7b48fe0159f67d5c00d83d1943f45c42
kareemelbeshlawy <132574693+kareemelbeshlawy@users.noreply.github.com>
2023-05-05 18:31:06 +0300

    Add files via upload

---
+++ cars.js
@@ -0,0 +1,275 @@
+
+$(document).ready(function(){
+    $(".m1").click(function(){
+        $(".carm1").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".m2").click(function(){
+        $(".carm2").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".m3").click(function(){
+        $(".carm3").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".m4").click(function(){
+        $(".carm4").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".m5").click(function(){
+        $(".carm5").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".m6").click(function(){
+        $(".carm6").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".m7").click(function(){
+        $(".carm7").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".m8").click(function(){
+        $(".carm8").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".m9").click(function(){
+        $(".carm9").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b1").click(function(){
+        $(".car1").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b2").click(function(){
+        $(".car2").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b3").click(function(){
+        $(".car3").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b4").click(function(){
+        $(".car4").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".b5").click(function(){
+        $(".car5").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".b6").click(function(){
+        $(".car6").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".o1").click(function(){
+        $(".caro1").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".o2").click(function(){
+        $(".caro2").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".o3").click(function(){
+        $(".caro3").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".o4").click(function(){
+        $(".caro4").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".o5").click(function(){
+        $(".caro5").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".o6").click(function(){
+        $(".caro6").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".i1").click(function(){
+        $(".cari1").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".i2").click(function(){
+        $(".cari2").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".i3").click(function(){
+        $(".cari3").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".i4").click(function(){
+        $(".cari4").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".i5").click(function(){
+        $(".cari5").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".i6").click(function(){
+        $(".cari6").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".p1").click(function(){
+        $(".carp1").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".p2").click(function(){
+        $(".carp2").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".p3").click(function(){
+        $(".carp3").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".p4").click(function(){
+        $(".carp4").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".p5").click(function(){
+        $(".carp5").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".p6").click(function(){
+        $(".carp6").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".p7").click(function(){
+        $(".carp7").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".p8").click(function(){
+        $(".carp8").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".a1").click(function(){
+        $(".cara1").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".a2").click(function(){
+        $(".cara2").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".a3").click(function(){
+        $(".cara3").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".a4").click(function(){
+        $(".cara4").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".a5").click(function(){
+        $(".cara5").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".a6").click(function(){
+        $(".cara6").toggle(1000);
+    });
+});
+
+
+
+
+
